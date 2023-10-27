d7e8f12f529e5aebaa8869662e4cf2f2c540a3ab
Basheer Jarrah <78933834+JARRAH2000@users.noreply.github.com>
2023-10-27 13:18:00 +0300

    Update ModelContext.cs

--- BusTracking.Core/Data/ModelContext.cs
+++ BusTracking.Core/Data/ModelContext.cs
@@ -55,7 +55,7 @@ public partial class ModelContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseOracle("User Id=JOR15_User76;PASSWORD=Test321;DATA SOURCE=94.56.229.181:3488/traindb");
+        => optionsBuilder.UseOracle("credentials");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
